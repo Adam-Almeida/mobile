1793a940856f74feeff4f3df05c21851de94384c
adam-almeida <adamportifolio@gmail.com>
2023-01-21 00:26:56 -0300

    mobile styles create

---
+++ src/screens/Home.tsx
@@ -0,0 +1,51 @@
+import { Text, View, ScrollView } from "react-native";
+import { HabitDay, DAY_SIZE } from "../components/HabitDay";
+import { Header } from "../components/Header";
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
+
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+const datesFromYearsStart = generateDatesFromYearBeginning();
+const minimumSummaryDateSize = 18 * 5;
+const amountOfDaysToFill = minimumSummaryDateSize - datesFromYearsStart.length;
+
+export function Home() {
+  return (
+    <View className="bg-background px-8 pt-16 flex-1">
+      <Header />
+      <View className="flex-row mt-6 mb-2">
+        {weekDays.map((day, i) => {
+          return (
+            <Text
+              className="text-zinc-400 text-xl font-bold text-center mx-1"
+              style={{ width: DAY_SIZE }}
+              key={`${day}-${i}`}
+            >
+              {day}
+            </Text>
+          );
+        })}
+      </View>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {datesFromYearsStart.map((date) => {
+            return <HabitDay key={date.toISOString()} />;
+          })}
+
+          {amountOfDaysToFill > 0 &&
+            Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+              return (
+                <View
+                  className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                  style={{ width: DAY_SIZE, height: DAY_SIZE }}
+                  key={i}
+                />
+              );
+            })}
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
